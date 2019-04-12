6370aea0a4a6cafba5d21c164fc69cf200c26034
dkovalsikoski <45408039+dkovalsikoski@users.noreply.github.com>
2019-04-12 19:59:47 -0400

    Update listalinear.c

--- lista/ponteiros/listalinear.c
+++ lista/ponteiros/listalinear.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct *Node{
-    int valor;
-    struct Node *prox;
+typedef struct Node{
+        int valor;
+        struct Node *prox;
 }no;
 
-typedef struct *Lista{
-    no *inicio;
-    no *fim;
+typedef struct Lista{
+        no *inicio;
+        no *fim;
 }lista;
 
-void lista_vazio(Lista *lista){
+void lista_vazio(lista *lista){
     lista->inicio = NULL;
     lista->fim = NULL;
 }
 
-void insere_lista_vazia(Lista *lista, int valor){
+void insere_lista_vazia(lista *lista, int valor){
     //aloca espaço para o novo nó
-    list->inicio=malloc(sizeof(no));
+    lista->inicio=malloc(sizeof(no));
     //define o ponteiro lista inicio para nulo
-    list->inicio->prox=NULL;
+    lista->inicio->prox=NULL;
     //recebe o valor
-    list->inicio->valor=valor;
-    //diz que o lista fim é o mesmo que o incio porque só tem ele 
-    list->fim=list->inicio;
+    lista->inicio->valor=valor;
+    //diz que o lista fim é o mesmo que o incio porque só tem ele
+    lista->fim=lista->inicio;
 }
 
-void insere_inicio(Lista *lista, int valor){
-    if(list->inicio==NULL){
+void insere_inicio(lista *lista, int valor){
+    if(lista->inicio==NULL){
         insere_lista_vazia(lista, valor);
     } else {
         //aloca espaço pro novo nó
@@ -36,35 +36,52 @@ void insere_inicio(Lista *lista, int valor){
         //define um valor
         novoNo->valor=valor;
         //novo nó aponta para lista inicio
-        novoNo->prox=list->inicio;
+        novoNo->prox=lista->inicio;
         //define o novo nó como lista inicio
-        list->inicio=novoNo;
+        lista->inicio=novoNo;
     }
 }
-void insere_fim(Lista *lista, int valor){
-    if(list->inicio==NULL){
-        insere_lista_vazia(list,valor)
+void insere_fim(lista *lista, int valor){
+    if(lista->inicio==NULL){
+        insere_lista_vazia(lista,valor);
     } else {
         //aloca o novo nó
         no *novoNo = malloc(sizeof(no));
-        novoNo->prox=NULL;
+        novoNo->prox = NULL;
         novoNo->valor = valor;
+
         //cria um ponteiro de verificção que aponta para o inicio da lista
-        no *tmp = list->prox;
-        while(tmp->prox!=NULL){
+        no *tmp = lista->inicio->prox;
+
+        while (tmp->prox != NULL)
             //enquanto a parada aqui não for nula ele percorre
-            tmp=tmp->prox;
-            if(tmp->prox==NULL){
-                list->fim->prox=novoNo;
-                list->fim = novoNo;
-            }
-        }
+            tmp = tmp->prox;
+        tmp->prox = novoNo;
+        lista->fim->prox = novoNo;
+        lista->fim = novoNo;
+
+    }
+}
+
+
+
+void imprime_lista(lista lista){
+    no *tmp = lista.inicio;
+    while(tmp!=NULL){
+        printf("%d\n", tmp->valor);
+        tmp=tmp->prox;
+
     }
 }
 
 
 int main()
 {
-
+    lista lista;
+    lista_vazio(&lista);
+    insere_inicio(&lista, 3);
+    insere_inicio(&lista,4);
+    insere_fim(&lista, 7);
+    imprime_lista(lista);
     return 0;
 }
